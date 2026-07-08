5966f2b8c1e2f141f47ba2c1ce6bd47239e54b20
trainingtimegym <trainingtimegimnasio@gmail.com>
2026-07-07 21:50:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycby24mOk_6NL23GRioUcHzTANDjtqSk0bYtWEzYRxH3_Tg5dGRKsHs-twrvNWxWE5Bld/exec',
-  NOMBRE_GIMNASIO: 'Mi Gimnasio',
+  NOMBRE_GIMNASIO: 'TRAINING TIME',
   LINK_GRUPO_WHATSAPP: '' // opcional, ej: 'https://chat.whatsapp.com/ABC123XYZ' (Grupo > Datos del grupo > Invitar mediante enlace)
 };
  
